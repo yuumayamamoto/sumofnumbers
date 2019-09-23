848c4fefbd849492875d3d7a4b357aca6f6b51f3
yuumayamamoto <46611050+yuumayamamoto@users.noreply.github.com>
2019-09-22 16:28:59 -1000

    finished excersize
    finidshed excecise

---
+++ sumofnumbers.js
@@ -0,0 +1,35 @@
+const testNums = [1, 2, 3, 4];
+
+function sumFor(nums) {
+  let total = 0;
+  for (const num of nums) {
+    total += num;
+  }
+  return total;
+}
+
+console.log(sumFor(testNums));
+
+function sumWhile(nums) {
+  let total = 0;
+  let i = 0;
+  while (i < nums.length) {
+    total += nums[i];
+    i++;
+  }
+  return total;
+}
+
+console.log(sumWhile(testNums));
+
+function sumRecursion(nums) {
+  if (nums.length === 0) {
+    return 0;
+  } return nums[0] + sumRecursion((nums.slice(1, nums.length)));
+}
+console.log(sumRecursion(testNums));
+
+function sumTheSimpleWay(nums) {
+  return _.reduce(nums, function (memo, num) { return memo + num; }, 0);
+}
+console.log(sumTheSimpleWay(testNums));
